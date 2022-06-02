9fc8d924dac94eec7becf4928f4c5692f1cd9bf4
Douglas Ferreira <dfaferreira46@gmail.com>
2022-06-02 08:27:16 -0300

    Create fizz-buzz-algorithm.js

---
+++ fizz-buzz-problem/fizz-buzz-algorithm.js
@@ -0,0 +1,32 @@
+//Generate array of numbers
+const generateNumbersArray = (upperLimit) => {
+	let counter = 1;
+	let numbersArray = [];
+
+	while(counter <= upperLimit) {
+    numbersArray.push(counter);
+    counter++;
+  }
+  
+  return numbersArray;
+}
+
+//Helper functions for string generation based on divisibility
+const fizz = (num) => !(num % 3) ? "Fizz" : "";
+const buzz = (num) => !(num % 5) ? "Buzz" : "";
+
+//Generate the final array
+const generateFinalArray = (upperLimit) => {
+  const numbersArray = generateNumbersArray(upperLimit);
+  let finalArray = [];
+  
+  numbersArray.forEach(num => {
+		const displayValue = fizz(num) + buzz(num) || num;
+    finalArray.push(displayValue);
+  })
+  
+  return finalArray;
+ }
+
+//Print the final array
+console.log(generateFinalArray(100));
