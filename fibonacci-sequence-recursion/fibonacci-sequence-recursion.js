74304378d0862f5842c723cca50899bf1ffc26aa
Douglas Ferreira <dfaferreira46@gmail.com>
2022-07-07 18:32:51 -0300

    Create fibonacci-sequence-recursion.js

---
+++ fibonacci-sequence-recursion/fibonacci-sequence-recursion.js
@@ -0,0 +1,14 @@
+const generateFibonacciSequence = (len, i=0, arr=[1]) => {
+  
+  if(arr.length <= len){
+    arr.push(arr.at(-1) + i);
+    i = arr.at(-2);
+    generateFibonacciSequence(len, i, arr);
+  }
+  
+  return arr;
+};
+
+const fibonacciSequence = generateFibonacciSequence(5);
+
+console.log(fibonacciSequence);
