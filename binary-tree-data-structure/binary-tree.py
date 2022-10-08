b71662bda3b8b0e6e9346dd56725fc6b094c4012
Douglas Ferreira <dfaferreira46@gmail.com>
2022-10-08 20:33:45 -0300

    Update binary-tree.py

--- binary-tree-data-structure/binary-tree.py
+++ binary-tree-data-structure/binary-tree.py
@@ -8,6 +8,8 @@ def create_root(data):
 
 # inserting a node to the tree
 def insert_node(root, data):
+    
+    # function to create a node
     def create_node(data):
         return {
             "data": data,
@@ -15,13 +17,23 @@ def insert_node(root, data):
             "right": None
         }
     
+    # checks whether the data property points to a valid node
+    # if so, proceed with the insertion process, otherwise point the data property to the data parameter (else statment)
     if root["data"]:
+        
+        # check whether the data parameter is greater than the data property
+        # if so, proceed with the insertion process on the left node, otherwise proceed with the right node
         if data < root["data"]:
+            
+            # if the left node is None, point it to a newly created node
+            # otherwise call insert_node function on it
             if root["left"] is None:
                 root["left"] = create_node(data)
             else:
                 insert_node(root["left"], data)
         else:
+            # if the right node is None, point it to a newly created node
+            # otherwise call insert_node function on it
             if root["right"] is None:
                 root["right"] = create_node(data)
             else:
@@ -31,21 +43,65 @@ def insert_node(root, data):
 
 # traversing the tree (pre-order, in-order, and post-order) recursively
 def traverse_tree_recur(root, fn, order = "pre_order"):
+    
+    # if order is set to pre-order, execute the fn paramenter at this point (tipical behavior of a pre-order traversal)
     if order == "pre_order": fn(root["data"])
     
+    # proceed visiting the left node
     if root["left"]:
         traverse_tree_recur(root["left"], fn, order)
     
+    # if order is set to in-order, execute the fn paramenter at this point (tipical behavior of a in-order traversal)
     if order == "in_order": fn(root["data"])
     
+    # proceed visiting the right node
     if root["right"]:
         traverse_tree_recur(root["right"], fn, order)
     
+    # if order is set to post-order, execute the fn paramenter at this point (tipical behavior of a pre-order traversal)
     if order == "post_order": fn(root["data"])
 
 # traversing the tree (pre-order, in-order, and post-order) iteratively
-def traverse_tree_recur(root, fn, order = "pre_order"):
-    #...
+def traverse_tree_iterate(root, fn, order = "pre_order"):
+    current = root
+    stack = []
+	
+    if order == "post_order":
+	    while current and current not in stack:
+	        if current["left"] and current["left"] not in stack:
+	            current = current["left"]
+	        elif current["right"] and current["right"] not in stack:
+	            current = current["right"]
+	        else:
+	            fn(current["data"])
+	            stack.append(current)
+	            current = root
+	    
+    else:
+        while True:
+    		
+    		# Reach the left most Node of the current Node
+	        if current is not None:
+    			
+    			# Place pointer to a tree node on the stack
+    			# before traversing the node's left subtree
+	            if order == "pre_order": fn(current["data"])
+	            stack.append(current)
+	            current = current["left"]
+    		
+    		# BackTrack from the empty subtree and visit the Node
+    		# at the top of the stack; however, if the stack is
+    		# empty you are done
+	        elif(stack):
+	            current = stack.pop()
+	            if order == "in_order": fn(current["data"])
+    		
+    			# We have visited the node and its left
+    			# subtree. Now, it's right subtree's turn
+	            current = current["right"]
+    
+	        else:
+	            break
 
 # printing tree nodes
 def print_node(node):
@@ -53,8 +109,9 @@ def print_node(node):
 
 # testing algorithms
 # inserting nodes (sub-trees)
-root = create_root(12)
-nodes = [24, 27, 23, 26, 28, 29]
+root = create_root(10)
+# nodes = [24, 27, 23, 26, 28, 29]
+nodes = [8, 7, 12]
 
 for node in nodes:
     insert_node(root, node)
